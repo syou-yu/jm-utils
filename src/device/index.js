7fa9bce6c88a5202df20780909028f8b451686d8
syou_yu <921393502@qq.com>
2020-11-25 16:19:45 +0800

    docs(device): 调整 js doc 描述

--- src/device/index.js
+++ src/device/index.js
@@ -1,9 +1,8 @@
-/**
- * 设备判断的工具函数
- */
+// 设备判断的工具函数
 
 /**
  * 确定设备是 移动设备 还是 台式机/笔记本电脑
+ * @return {string} 'Mobile'/'Desktop'
  */
 export function deviceType() {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
